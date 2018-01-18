eeb5420cac2d24d6c19f99b82b09a742bc7b4c63
Juan Giron <gironmolina@gmail.com>
2018-01-18 17:29:23 +0100

    changes

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,16 +1,33 @@
-import { Component } from "@angular/core";
+
+import { IProduct } from "./product";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-selector: "pm-products",
-templateUrl: "./product-list.component.html"
+    selector: "pm-products",
+    templateUrl: "./product-list.component.html",
+    styleUrls: ["./product-list.component.css"]
 })
 
-export class ProductListComponent {
-    pageTitle: string = "Product List";
+export class ProductListComponent implements OnInit {        
+        pageTitle: string = "Product List";
     imageWidth: number = 50;
     imageMargin:number = 2;
     showImage:boolean = false;
-    products: any[] = [
+
+    _listFilter:string;
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value:string) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ?
+            this.performFilter(this.listFilter) :
+            this.products;
+    }
+
+
+    filteredProducts: IProduct[];
+    products: IProduct[] = [
         {
             "productId": 2,
             "productName": "Garden Cart",
@@ -33,7 +50,22 @@ export class ProductListComponent {
         }
     ];
 
+    constructor(){
+        this.filteredProducts = this.products;
+        this.listFilter = "cart";
+    }
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
+
+    ngOnInit(): void {
+        console.log("In OnInit");
+    }    
 }
