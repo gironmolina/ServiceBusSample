9f4013d17f0166cfafc76ae6b1552fe17c2e196b
Huans Hirons <gironmolina@gmail.com>
2018-01-21 23:55:20 +0100

    changes

--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -2,10 +2,11 @@ import { Injectable } from "@angular/core";
 import { IProduct } from "./product";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs/Observable";
+import { HttpErrorResponse } from "@angular/common/http/src/response";
 import "rxjs/add/observable/throw";
 import "rxjs/add/operator/catch";
 import "rxjs/add/operator/do";
-import { HttpErrorResponse } from "@angular/common/http/src/response";
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
@@ -17,6 +18,11 @@ export class ProductService {
         return this._http.get<IProduct[]>(this._productUrl)
             .do(data => console.log("All: " + JSON.stringify(data)))
             .catch(this.handleError);
+    }
+
+    getProduct(id: number): Observable<IProduct> {
+        return this.getProducts()
+            .map((products: IProduct[]) => products.find(p => p.productId === id));
     }
 
     private handleError(err: HttpErrorResponse) {
